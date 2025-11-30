7ba15169f154cb72134c4bbade4f0b7cea7db135
m-brtn <ruslanchek@gmail.com>
2025-11-30 20:34:22 +0200

    Update root endpoint message to remove redundant information and streamline API description

--- backend/main.py
+++ backend/main.py
@@ -77,7 +77,7 @@ app.include_router(project_router)
 
 @app.get("/")
 async def root():
-    return {"message": "Hi there! This is Locales API. Visit https://keeys.app to FastAPI + GraphQL Boilerplate! for more information."}
+    return {"message": "Hi there! This is Locales API. Visit https://keeys.app for more information."}
 
 
 @app.get("/health")
